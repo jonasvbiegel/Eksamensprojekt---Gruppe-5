1f7cd3f640e65f526411331933f4c2634dfbaaa5
jonasvbiegel <167175411+jonasvbiegel@users.noreply.github.com>
2024-05-27 09:54:01 +0200

    Update Product.java

--- SemesterprojektGruppe5/model/Product.java
+++ SemesterprojektGruppe5/model/Product.java
@@ -5,7 +5,7 @@ public class Product
     private String name;
     private String serialNo;
     private int quantityInStock;
-    /* Price list for keeping track of current and past prices
+    /* TODO Price list for keeping track of current and past prices
      * Needs "Price" class
      * Price prices = ArrayList<Price>
      */
@@ -15,4 +15,27 @@ public class Product
         this.serialNo = serialNo;
         quantityInStock = 0;
     }
+    
+    public String getName() {
+        return this.name; 
+    }
+    public String getSerialNo() { 
+        return this.serialNo; 
+    }
+    public int getQuantityInStock(){
+        return this.quantityInStock;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setSerialNo(String serialNo){
+        this.serialNo = serialNo;
+    }
+    public void setQuantityInStock(int quantity){
+        this.quantityInStock = quantity;
+    }
+    
+    /*
+     * TODO Price getters and setters
+     */
 }
