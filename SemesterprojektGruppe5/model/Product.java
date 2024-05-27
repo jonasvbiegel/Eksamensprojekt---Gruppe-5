531702cf03cd986128ade3378b3441bc42a4439e
jonasvbiegel <167175411+jonasvbiegel@users.noreply.github.com>
2024-05-27 09:09:51 +0200

    test

--- SemesterprojektGruppe5/model/Product.java
+++ SemesterprojektGruppe5/model/Product.java
@@ -5,6 +5,10 @@ public class Product
     private String name;
     private String serialNo;
     private int quantityInStock;
+    /* Price list for keeping track of current and past prices
+     * Needs "Price" class
+     * Price prices = ArrayList<Price>
+     */
     
     public Product(String name, String serialNo){
         this.name = name;
