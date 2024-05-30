60f074aabedc46582d000dd324005d00d79b5cad
jonasvbiegel <167175411+jonasvbiegel@users.noreply.github.com>
2024-05-30 12:57:44 +0200

    Big fix

--- SemesterprojektGruppe5/view/Main.java
+++
@@ -1,9 +0,0 @@
-package view;
-
-public class Main {
-    public static void main(String[] args) {
-        Test t = new Test();
-        t.generateTestData();
-        SaleUI s = new SaleUI();
-    }
-}
